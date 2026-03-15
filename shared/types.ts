710d292a967fee3a07f1276035f12d87643fe852
CMSOTO2 <carlos.m.soto2@gmail.com>
2026-03-15 09:44:47 -0400

    clean up csv's

--- shared/types.ts
+++ shared/types.ts
@@ -3,3 +3,17 @@ export interface GDPData {
   year: number;
   gdp: number;
 }
+
+export interface EconomicHistory {
+  country_code: string;
+  country_name: string;
+  region: string;
+  income: string;
+  year: number;
+  gdp_growth: number;
+  inflation: number;
+  life_expectancy: number;
+  poverty: number;
+}
+
+export interface EconomicLatest extends EconomicHistory {}
